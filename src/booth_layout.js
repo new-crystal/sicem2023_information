31908813d5ce4a7637fad401324468b684e74a44
new-crystal <oj3339@naver.com>
2023-10-24 18:32:26 +0900

    final

--- src/booth_layout.js
+++ src/booth_layout.js
@@ -8,10 +8,19 @@ const bronze_menu_container = document.querySelector(".bronze_menu_container");
 const standard_menu_container = document.querySelector(".standard_menu_container");
 const sponsor_logo = document.querySelector(".sponsor_logo")
 const mark =  document.querySelector(".location");
+const img_3f = document.querySelector(".img_3f")
+const img_b1 = document.querySelector(".img_b1")
 const platinumSponsorList = document.querySelectorAll(".platinum_sponsor");
 const goldSponsorList = document.querySelectorAll(".gold_sponsor");
 const silverSponsorList = document.querySelectorAll(".silver_sponsor");
 const bronzeSponsorList = document.querySelectorAll(".bronze_sponsor")
+const standardSponsorList = document.querySelectorAll(".standard_sponsor")
+const footer = document.querySelector(".footer")
+
+
+footer.addEventListener("click", ()=>{
+  window.location.href = "index.html"
+})
 
 detailMenuList.forEach((detailMenu)=>{
     detailMenu.addEventListener("click", ()=>{
@@ -20,140 +29,199 @@ detailMenuList.forEach((detailMenu)=>{
 })
 
 sponsorMenu.addEventListener("click", (e)=>{
-    location.href = `?sponsor=${e.target.id}`
+    //  location.href = `?sponsor=${e.target.id}`
+    const grade = e.target.id;
+	sponsor_logo.removeAttribute('src');
+    if(grade === "platinum"){
+        backgroundImg.src = "./assets/booth_layout/booth_layout_p_1.png"
+        img_3f.style.display = "";
+        img_b1.style.display = "none";
+        mark.style.display = "none"
+
+        platinum_menu_container.style.display = ""
+        gold_menu_container.style.display = "none"
+        silver_menu_container.style.display = "none"
+        bronze_menu_container.style.display = "none"
+        standard_menu_container.style.display = "none"
+    }
+    if(grade === "gold"){
+        backgroundImg.src = "./assets/booth_layout/booth_layout_g_1.png"
+        img_3f.style.display = "";
+        img_b1.style.display = "none";
+        mark.style.display = "none"
+
+        platinum_menu_container.style.display = "none"
+        gold_menu_container.style.display = ""
+        silver_menu_container.style.display = "none"
+        bronze_menu_container.style.display = "none"
+        standard_menu_container.style.display = "none"
+    }
+    if(grade === "silver"){
+        backgroundImg.src = "./assets/booth_layout/booth_layout_s_1.png"
+        img_3f.style.display = "none";
+        img_b1.style.display = "";
+        mark.style.display = "none"
+
+        platinum_menu_container.style.display = "none"
+        gold_menu_container.style.display = "none"
+        silver_menu_container.style.display = ""
+        bronze_menu_container.style.display = "none"
+        standard_menu_container.style.display = "none"
+  
+    }
+    if(grade === "bronze"){
+        backgroundImg.src = "./assets/booth_layout/booth_layout_b_1.png"
+        img_3f.style.display = "none";
+        img_b1.style.display = "";
+        mark.style.display = "none"
+
+        platinum_menu_container.style.display = "none"
+        gold_menu_container.style.display = "none"
+        silver_menu_container.style.display = "none"
+        bronze_menu_container.style.display = ""
+        standard_menu_container.style.display = "none"
+    }
+    if(grade === "standard"){
+        backgroundImg.src = "./assets/booth_layout/booth_layout_etc_1.png"
+        img_3f.style.display = "none";
+        img_b1.style.display = "";
+        mark.style.display = "none"
+
+        platinum_menu_container.style.display = "none"
+        gold_menu_container.style.display = "none"
+        silver_menu_container.style.display = "none"
+        bronze_menu_container.style.display = "none"
+        standard_menu_container.style.display = ""
+    }
 })
 
-function changeBackgroundImg(){
-const sponsorGrade = new URLSearchParams(window.location.search).get("sponsor");
-const target =  new URLSearchParams(window.location.search).get("target");
-    if(location.search === ""){
-        backgroundImg.src = "./assets/booth_layout/booth_layout_p_1.png"
-        mark.style.display = "none";
-        platinum_menu_container.style.display = "";
-        gold_menu_container.style.display = "none";
-        silver_menu_container.style.display = "none";
-        bronze_menu_container.style.display = "none";
-        standard_menu_container.style.display = "none";
-    }
-    else if(sponsorGrade === "platinum"){
-        if(target){
-            backgroundImg.src = "./assets/booth_layout/booth_layout_p.png"
-            mark.style.display = "";
-            sponsor_logo.src = `./assets/sponsor/${target}.png`
-            platinumSponsorList.forEach((sponsor)=>{
-                if(sponsor.id === target){
-                    sponsor.style.backgroundImage = `url(./assets/sponsor/${target}_1.png)`;
-                }
-            })
-        }else{
-            backgroundImg.src = "./assets/booth_layout/booth_layout_p_1.png"
-            mark.style.display = "none";
-        }
-        platinum_menu_container.style.display = "";
-        gold_menu_container.style.display = "none";
-        silver_menu_container.style.display = "none";
-        bronze_menu_container.style.display = "none";
-        standard_menu_container.style.display = "none";
-    }
-    else if(sponsorGrade === "gold"){
-        if(target){
-            backgroundImg.src = "./assets/booth_layout/booth_layout_g.png"
-            mark.style.display = "";
-            sponsor_logo.src = `./assets/sponsor/${target}.png`
-            goldSponsorList.forEach((sponsor)=>{
-                if(sponsor.id === target){
-                    sponsor.style.backgroundImage = `url(./assets/sponsor/${target}_1.png)`;
-                }
-            })
-        }else{
-            backgroundImg.src = "./assets/booth_layout/booth_layout_g_1.png"
-            mark.style.display = "none";
-        }
-        platinum_menu_container.style.display = "none";
-        gold_menu_container.style.display = "";
-        silver_menu_container.style.display = "none";
-        bronze_menu_container.style.display = "none";
-        standard_menu_container.style.display = "none";
-    }
-    else if(sponsorGrade === "silver"){
-        if(target){
-            backgroundImg.src = "./assets/booth_layout/booth_layout_s.png"
-            mark.style.display = "";
-            sponsor_logo.src = `./assets/sponsor/${target}.png`
-            silverSponsorList.forEach((sponsor)=>{
-                if(sponsor.id === target){
-                    sponsor.style.backgroundImage = `url(./assets/sponsor/${target}_1.png)`;
-                }
-            })
-        }else{
-            backgroundImg.src = "./assets/booth_layout/booth_layout_s_1.png"
-            mark.style.display = "none";
-        }
-        silver_menu_container.style.display = "";
-        platinum_menu_container.style.display = "none";
-        gold_menu_container.style.display = "none";
-        bronze_menu_container.style.display = "none";
-        standard_menu_container.style.display = "none";
-    }
-    else if(sponsorGrade === "bronze"){
-        if(target){
-            backgroundImg.src = "./assets/booth_layout/booth_layout_b.png"
-            bronzeSponsorList.forEach((sponsor)=>{
-                if(sponsor.id === target){
-                    sponsor.style.backgroundImage = `url(./assets/sponsor/${target}_1.png)`;
-                }
-            })
-            mark.style.display = "";
-            sponsor_logo.src = `./assets/sponsor/${target}.png`
-        }else{
-            backgroundImg.src = "./assets/booth_layout/booth_layout_b_1.png"
-            mark.style.display = "none";
-        }
-        bronze_menu_container.style.display = "";
-        platinum_menu_container.style.display = "none";
-        gold_menu_container.style.display = "none";
-        silver_menu_container.style.display = "none";
-        standard_menu_container.style.display = "none";
-    }
-    else if(sponsorGrade === "standard"){
-        if(target){
-            backgroundImg.src = "./assets/booth_layout/booth_layout_etc_1.png"
-            mark.style.display = "";
-            sponsor_logo.src = `./assets/sponsor/${target}.png`
-        }else{
-            backgroundImg.src = "./assets/booth_layout/booth_layout_etc_1.png"
-            mark.style.display = "none";
-        }
-        standard_menu_container.style.display = "";
-        platinum_menu_container.style.display = "none";
-        gold_menu_container.style.display = "none";
-        silver_menu_container.style.display = "none";
-        bronze_menu_container.style.display = "none";
-    }
+const loadBoothLayout = ()=>{
+    backgroundImg.src = "./assets/booth_layout/booth_layout_p_1.png"
+    gold_menu_container.style.display = "none"
+    silver_menu_container.style.display = "none"
+    bronze_menu_container.style.display = "none"
+    standard_menu_container.style.display = "none"
+    img_3f.style.display = "";
+    img_b1.style.display = "none";
+    mark.style.display = "none"
 }
 
+
 platinum_menu_container.addEventListener("click", (e)=>{
-    location.href = `?sponsor=platinum&target=${e.target.id}`
+
+
+    const target = e.target.id;
+    if(target){
+        backgroundImg.src = "./assets/booth_layout/booth_layout_p.png"
+        img_3f.style.display = "";
+        img_b1.style.display = "none";
+
+        mark.style.display = "";
+        mark.classList.replace(mark.classList[0],target)
+        sponsor_logo.src = `./assets/sponsor/${target}.png`
+        
+        platinumSponsorList.forEach((sponsor)=>{
+            if(sponsor.id === target){
+                sponsor.style.backgroundImage = `url(./assets/sponsor/${target}_1.png)`;
+            }else{
+                sponsor.style.backgroundImage = ``;
+            }
+        })
+    }
 })
 
 gold_menu_container.addEventListener("click", (e)=>{
-    location.href = `?sponsor=gold&target=${e.target.id}`
+    const target = e.target.id;
+
+    if(target){
+        backgroundImg.src = "./assets/booth_layout/booth_layout_g.png"
+
+        if(target !== "g_6" && target !== "g_7" && target !== "g_8" ){
+            img_3f.style.display = "";
+            img_b1.style.display = "none";
+        }
+        else{
+            img_3f.style.display = "none";
+            img_b1.style.display = "";
+        }
+        mark.style.display = "";
+        mark.classList.replace(mark.classList[0],target)
+        sponsor_logo.src = `./assets/sponsor/${target}.png`
+        goldSponsorList.forEach((sponsor)=>{
+            if(sponsor.id === target){
+                sponsor.style.backgroundImage = `url(./assets/sponsor/${target}_1.png)`;
+            }else{
+                sponsor.style.backgroundImage = ``;
+            }
+        })
+    }
 })
 
 silver_menu_container.addEventListener("click", (e)=>{
-    location.href = `?sponsor=silver&target=${e.target.id}`
+       
+    const target = e.target.id;
+    if(target){
+        backgroundImg.src = "./assets/booth_layout/booth_layout_s.png"
+            img_3f.style.display = "none";
+            img_b1.style.display = "";
+    
+        mark.style.display = "";
+        mark.classList.replace(mark.classList[0],target)
+        sponsor_logo.src = `./assets/sponsor/${target}.png`
+        silverSponsorList.forEach((sponsor)=>{
+            if(sponsor.id === target){
+                sponsor.style.backgroundImage = `url(./assets/sponsor/${target}_1.png)`;
+            }else{
+                sponsor.style.backgroundImage = ``;
+            }
+        })
+    }
 })
 
 bronze_menu_container.addEventListener("click", (e)=>{
-    location.href = `?sponsor=bronze&target=${e.target.id}`
+        
+    const target = e.target.id;
+    if(target){
+        backgroundImg.src = "./assets/booth_layout/booth_layout_b.png"
+            img_3f.style.display = "none";
+            img_b1.style.display = "";
+    
+        mark.style.display = "";
+        mark.classList.replace(mark.classList[0],target)
+        sponsor_logo.src = `./assets/sponsor/${target}.png`
+        bronzeSponsorList.forEach((sponsor)=>{
+            if(sponsor.id === target){
+                sponsor.style.backgroundImage = `url(./assets/sponsor/${target}_1.png)`;
+            }else{
+                sponsor.style.backgroundImage = ``;
+            }
+        })
+    }
 })
 
 standard_menu_container.addEventListener("click", (e)=>{
-    location.href = `?sponsor=standard&target=${e.target.id}`
+            
+    const target = e.target.id;
+    if(target){
+        backgroundImg.src = "./assets/booth_layout/booth_layout_etc.png"
+            img_3f.style.display = "none";
+            img_b1.style.display = "";
+    
+        mark.style.display = "";
+        mark.classList.replace(mark.classList[0],target)
+        sponsor_logo.src = `./assets/sponsor/${target}.png`
+        standardSponsorList.forEach((sponsor)=>{
+            if(sponsor.id === target){
+                sponsor.style.backgroundImage = `url(./assets/sponsor/${target}_1.png)`;
+            }else{
+                sponsor.style.backgroundImage = ``;
+            }
+        })
+    }
 })
 
+window.onload = loadBoothLayout()
 
-window.onload = changeBackgroundImg()
 
     /**우클릭 방지 */
     document.addEventListener("contextmenu", function(event) {
--- src/sicem_information.js
+++ src/sicem_information.js
@@ -2,6 +2,24 @@ const detailMenuList = document.querySelectorAll(".detail_menu");
 const info_menu_container = document.querySelector(".info_menu_container")
 const info_background = document.querySelector(".info_background")
 const contentList = document.querySelectorAll(".content")
+const history_pre_btn = document.querySelector(".history_pre");
+const history_next_btn = document.querySelector(".history_next");
+const history_img = document.querySelector(".history_img")
+const carousel = document.querySelector(".carousel")
+const footer = document.querySelector(".footer")
+const wifi_info = document.querySelector(".wifi_info")
+const preview_room = document.querySelector(".preview_room")
+const registration_desk = document.querySelector(".registration_desk")
+const poster = document.querySelector(".poster")
+const prayer_room = document.querySelector(".prayer_room")
+const history = document.querySelector(".history")
+
+let index = 0;
+let history_interval;
+
+footer.addEventListener("click", ()=>{
+    window.location.href = "index.html"
+})
 
 detailMenuList.forEach((detailMenu)=>{
     detailMenu.addEventListener("click", ()=>{
@@ -10,71 +28,74 @@ detailMenuList.forEach((detailMenu)=>{
 })
 
 info_menu_container.addEventListener("click",(e)=>{
-    location.href = `?info=${e.target.id}`
+ 
+    if(e.target.id === "history"){
+        info_background.src = `./assets/sicem_info/Sinfo_${e.target.id}_1.png`
+		index = 0;
+		carousel.style.transform = `translate3d(0px, 0, 0)`;
+		
+        history.style.display = "";
+    }
+    else if(e.target.id){
+        info_background.src = `./assets/sicem_info/Sinfo_${e.target.id}.png`
+        history.style.display = "none";
+    }
 })
 
-function changeBackgroundImage(){
-    const infoMenu =  new URLSearchParams(window.location.search).get("info");
-    contentList.forEach((content)=>{
-        content.style.display = "none"
-    })
 
-    if(location.search === ""){
-        info_background.src = "./assets/sicem_info/Sinfo_wifi.png"
-        contentList.forEach((content)=>{
-            if(content.classList.value.includes("wifi_info")){
-                content.style.display = "";
-            }
-        })
+function changeBackgroundImg(){
+    if(index === 0){
+        info_background.src = "./assets/sicem_info/Sinfo_history_1.png"
     }
-    else if(infoMenu === "wifi"){
-        info_background.src = "./assets/sicem_info/Sinfo_wifi.png"
-        contentList.forEach((content)=>{
-            if(content.classList.value.includes("wifi_info")){
-                content.style.display = "";
-            }
-        })
+    else if (index === 3){
+        info_background.src = "./assets/sicem_info/Sinfo_history_2.png"
     }
-    else if(infoMenu === "preview"){
-        info_background.src = "./assets/sicem_info/Sinfo_preview.png"
-        contentList.forEach((content)=>{
-            if(content.classList.value.includes("preview_room")){
-                content.style.display = "";
-            }
-        })
-    }
-    else if(infoMenu === "registration"){
-        info_background.src = "./assets/sicem_info/Sinfo_regi.png"
-        contentList.forEach((content)=>{
-            if(content.classList.value.includes("registration_desk")){
-                content.style.display = "";
-            }
-        })
-    }
-    else if(infoMenu === "poster"){
-        info_background.src = "./assets/sicem_info/Sinfo_poster.png"
-        contentList.forEach((content)=>{
-            if(content.classList.value.includes("poster")){
-                content.style.display = "";
-            }
-        })
-    }
-    else if(infoMenu === "prayer"){
-        info_background.src = "./assets/sicem_info/Sinfo_prayer.png"
-        contentList.forEach((content)=>{
-            if(content.classList.value.includes("prayer_room")){
-                content.style.display = "";
-            }
-        })
-        
-    }
-    else if(infoMenu === "history"){
+    else{
         info_background.src = "./assets/sicem_info/Sinfo_history.png"
     }
 }
 
+history_pre_btn.addEventListener("click", ()=>{
+    preImg()
+    changeBackgroundImg()})
 
-window.onload = changeBackgroundImage()
+history_next_btn.addEventListener("click", ()=>{
+    nextImg()
+    changeBackgroundImg()})
+
+// /**자동으로 이미지 넘기기 */
+// function autoNextImg(){
+//     history_interval = setInterval(()=>{
+//         nextImg()
+//     },5000)
+// }
+
+/**이미지 다음으로 넘기기 */
+function nextImg(){
+ if (index !== 3){
+        index +=1
+    }
+
+    carousel.style.transform = `translate3d(-${
+        980 * index
+      }px, 0, 0)`;
+}
+
+/**이미지 이전으로 넘기기 */
+function preImg(){
+ if(index !== 0){
+        index -= 1;
+    }
+    carousel.style.transform = `translate3d(-${
+        980 * index
+      }px, 0, 0)`;
+  
+}
+
+
+window.addEventListener("beforeunload", ()=>{
+    clearInterval(history_interval)
+})
 
     /**우클릭 방지 */
     document.addEventListener("contextmenu", function(event) {
